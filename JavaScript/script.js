65acd842fbc9e35778a188f8d9d5bf223f3ea45a
Kartavya Badgujar <83571014+Devoir23@users.noreply.github.com>
2024-10-11 20:26:41 +0530

    map function

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,26 +1,26 @@
-// // Array
+/*Array
 
-// let arr = ["cea","rr",2,3,4,true,null];
-// console.log(arr.length);
+let arr = ["cea","rr",2,3,4,true,null];
+console.log(arr.length);
 
-// // console.log("before push: ", arr);
+console.log("before push: ", arr);
 
-// // push()
-// // arr.push("Kartavya")
-// // console.log("after push: ", arr);
+push()
+arr.push("Kartavya")
+console.log("after push: ", arr);
 
-// // unshifted
-// // arr.unshift("Kartavya");
-// // console.log("unshift: ", arr);
+unshifted
+arr.unshift("Kartavya");
+console.log("unshift: ", arr);
 
-// //splice-> remove element 
-// console.log("before splice: ", arr);
-// arr.splice(1,3);
-// console.log("after splice: ",arr);
-// console.log(arr.length);
+splice-> remove element 
+console.log("before splice: ", arr);
+arr.splice(1,3);
+console.log("after splice: ",arr);
+console.log(arr.length);
 
 
-// console.log(arr);
+console.log(arr);
 
 
 let num = [10,20,30,40];
@@ -29,5 +29,16 @@ let num = [10,20,30,40];
 console.log(num.indexOf(30)); //indexof
 console.log(num.reverse());
 console.log(num.sort());
+*/
 
 
+// map function
+
+const num = [10,20,30,40,50,70,80,90];
+
+const detailsMap = num.map((num,index)=>{
+    return `${index} : ${num}`
+}) 
+
+console.log(detailsMap);
+console.log(num);
