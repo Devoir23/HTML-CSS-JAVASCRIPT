8132c6582db2b35ef8eb9e14d24ed5d02bf3d47a
Kartavya Badgujar <83571014+Devoir23@users.noreply.github.com>
2024-10-11 21:47:24 +0530

    selectors-DOM

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,58 +1,24 @@
-/*Array
-
-let arr = ["cea","rr",2,3,4,true,null];
-console.log(arr.length);
-
-console.log("before push: ", arr);
-
-push()
-arr.push("Kartavya")
-console.log("after push: ", arr);
-
-unshifted
-arr.unshift("Kartavya");
-console.log("unshift: ", arr);
-
-splice-> remove element 
-console.log("before splice: ", arr);
-arr.splice(1,3);
-console.log("after splice: ",arr);
-console.log(arr.length);
+// getElementByTagName()
+var a = document.getElementsByTagName('div')
+console.log(a);
 
 
-console.log(arr);
+// getElementByClassName()
+var b = document.getElementsByClassName('actor')
+console.log(b);
 
 
-let num = [10,20,30,40];
+// getElementById()
+var c = document.getElementById('names')
+console.log(c);
 
 
-console.log(num.indexOf(30)); //indexof
-console.log(num.reverse());
-console.log(num.sort());
-*/
+// querySelector()
+var d = document.querySelector('.actor')
+console.log(d);
 
 
-/* map function
 
-const num = [10,20,30,40,50,70,80,90];
-
-const detailsMap = num.map((num,index)=>{
-    return `${index} : ${num}`
-}) 
-
-console.log(detailsMap);
-console.log(num); */
-
-// forEach
-const num = [10,20,30,40,50,70,80,90];
-
-num.forEach((num) => {
-    square = num * num
-    console.log(square)
-})
-
-let detailEach = num.forEach((num) => {
-    square = num * num
-    return square
-})
-console.log(detailEach) 
+// querySelectorAll()
+var e = document.querySelectorAll('actor')
+console.log(e);
