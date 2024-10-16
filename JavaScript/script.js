7e7593213d0f5c9c37f1249b8d25e125f7a571a3
Kartavya Badgujar <83571014+Devoir23@users.noreply.github.com>
2024-10-16 14:12:24 +0530

    append child

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,12 +1,20 @@
 // Traversal DOM
 
 // Parent Element 
-var parent = document.querySelector('div')
-var p = parent.parentElement;
-console.log(p);
+// var parent = document.querySelector('div')
+// var p = parent.parentElement;
+// console.log(p);
 
 // childrenElemt
-var child = document.querySelector('ul')
-var c = child.children;
-console.log(c);
+// var child = document.querySelector('ul')
+// var c = child.children;
+// console.log(c);
 
+// previousElementSibling
+// console.log(document.querySelector('.hero').previousElementSibling);
+
+// append child 
+const ul = document.querySelector('ul');
+const newElement = document.createElement('li');
+newElement.textContent = "Append child";
+ul.appendChild(newElement)
