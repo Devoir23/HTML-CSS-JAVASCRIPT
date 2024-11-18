cb2c19e34f0a21780beec0dce26f03c558a9d83a
Kartavya Badgujar <83571014+Devoir23@users.noreply.github.com>
2024-11-18 23:32:49 +0530

    component and jsx

---
+++ React/react-series/src/App.js
@@ -0,0 +1,16 @@
+// Component
+// import { Fragment } from "react";
+import './App.css';
+
+function App(){
+    let a =2;
+    return(
+        <div className="hero">
+        <h1 style={{color: 'red'}}>Component and jsx</h1> {/*jsx*/}
+        <p>{a}</p>
+        </div>
+        
+    )
+}
+
+export default App;
