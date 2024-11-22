a3c0bbef6335e9000d426e584a93905d57d43eb3
Kartavya Badgujar <83571014+Devoir23@users.noreply.github.com>
2024-11-22 19:48:21 +0530

    fecthing data using ueEffect

--- React/react-series/src/App.js
+++ React/react-series/src/App.js
@@ -1,21 +1,44 @@
-// learning about useEffect
-import React, { useState, useEffect } from "react";
+// fetching data using useEffect
+import React, { useState, useEffect }  from "react";
 
-function  App(){
-  const [count, setCount] = useState(0);
+function DataFetching(){
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
 
-  // using useEffect to log a message
   useEffect(() => {
-    console.log('count updated: ${count}');
-  }, [count]);
+    // fetching data from API
+    const fetchData = async () => {
+      try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        if(!response.ok){
+          throw new Error('Failed to fetch data');
+        }
+        const result = await response.json();
+        setData(result.slice(0,5)); // display only the first 5 posts
+      }
+      catch (err){
+        setError(err.message);
+      }
+    };
+
+    fetchData();
+
+  }, []); // empty dependency array ensures it runs once on mount 
+  if (error) return <p>Error: {error}</p>;
 
   return(
-    <dv>
-      <h1>React useEffect Example</h1>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>+</button>
-    </dv>
+    <div>
+    <h1>Fetched Data</h1>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <strong>{item.title}</strong>
+            <p>{item.body}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
-export default App;
+export default DataFetching;
